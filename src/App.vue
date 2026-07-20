3bb0fce27d8501156d1c981f093af4f6027d6f98
zhouyucong <3079452260@qq.com>
2026-07-21 02:46:54 +0800

    引入字体图标

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 
 <template>
   <!-- 一级路由出口组件 -->
-  <div class="test">我上早八</div>
+  <!-- <div class="test">我上早八</div> -->
   <router-view />
 </template>
 
 <style scoped lang="scss">
-.test {
-  color: $xtxColor;
-}
+// .test {
+//   color: $xtxColor;
+// }
 </style>
