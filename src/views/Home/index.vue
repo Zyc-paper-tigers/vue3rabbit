1e0f545cd6809d6464439f7b395638ec7fd4bed5
zhouyucong <3079452260@qq.com>
2026-07-21 05:17:15 +0800

    面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from "./components/HomeBanner.vue";
 import HomeNew from "./components/HomeNew.vue";
 import HomeHot from "./components/HomeHot.vue";
 import HomeProduct from "./components/HomeProduct.vue";
+import HomePanel from "./components/HomePanel.vue";
 </script>
 
 <template>
@@ -14,4 +15,12 @@ import HomeProduct from "./components/HomeProduct.vue";
   <HomeNew />
   <HomeHot />
   <HomeProduct />
+
+  <!-- 在父组件使用的插槽中定义相应的属性名，即可将值传到子组件的props中 -->
+  <HomePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
+    <div>我是插槽内容</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" subTitle="人气爆款 品质靠谱">
+    <div>我是插槽内容</div>
+  </HomePanel>
 </template>
