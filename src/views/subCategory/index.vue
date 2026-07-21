630231dcdec06ebf75d75e54d9a006c28b7d5f3f
zhouyucong <3079452260@qq.com>
2026-07-21 23:05:17 +0800

    二级分类基础列表功能排序及无限加载实现

--- src/views/subCategory/index.vue
+++ src/views/subCategory/index.vue
@@ -26,13 +26,48 @@ const reqData = ref({
 });
 const getGoodList = async () => {
   const res = await getSubCategoryAPI(reqData.value);
-  console.log(res);
+  // console.log(res);
   goodList.value = res.data.result.items;
 };
 
 onMounted(() => {
   getGoodList();
 });
+
+const tabChange = () => {
+  // 成功触发
+  // console.log(reqData.value.sortField);
+  reqData.value.page = 1;
+  finished.value = false;
+  getGoodList();
+};
+
+// el-scrollbar 在滚动到底部时触发加载，替代即将弃用的 v-infinite-scroll 指令
+const loading = ref(false);
+const finished = ref(false);
+const load = async () => {
+  if (loading.value || finished.value) return;
+
+  loading.value = true;
+  try {
+    // 获取下一页的数据
+    reqData.value.page++;
+    const res = await getSubCategoryAPI(reqData.value);
+    const items = res.data.result.items;
+    // 拼接
+    goodList.value = [...goodList.value, ...items];
+    // 加载完毕，停止继续请求
+    if (items.length === 0) {
+      finished.value = true;
+    }
+  } finally {
+    loading.value = false;
+  }
+};
+
+const handleEndReached = (direction) => {
+  if (direction === "bottom") load();
+};
 </script>
 
 <template>
@@ -48,15 +83,17 @@ onMounted(() => {
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
-        <!-- 商品列表-->
-        <GoodsItem v-for="item in goodList" :key="item.id" :goods="item" />
-      </div>
+      <el-scrollbar class="goods-scrollbar" max-height="70vh" :distance="100" @end-reached="handleEndReached">
+        <div class="body">
+          <!-- 商品列表-->
+          <GoodsItem v-for="item in goodList" :key="item.id" :goods="item" />
+        </div>
+      </el-scrollbar>
     </div>
   </div>
 </template>
@@ -75,6 +112,10 @@ onMounted(() => {
     display: flex;
     flex-wrap: wrap;
     padding: 0 10px;
+  }
+
+  .goods-scrollbar {
+    margin: 0 -10px;
   }
 
   .goods-item {
